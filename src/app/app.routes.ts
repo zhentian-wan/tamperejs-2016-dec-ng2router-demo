3622007588b9aaeb72de28ebc6fe4eb18176ca38
Zhentian Wan <answer881215@gmail.com>
2016-12-08 15:19:54 +0200

    update using hash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,17 +6,16 @@ const indexRoute = {path: '', redirectTo: 'home', pathMatch: 'full'};
 const fallbackRoute = {path: '**', component: E404Component, data: {title: 'Page not found'}};
 
 const routes = [
-  {path: 'index', redirectTo: '/home', pathMatch: 'prefix'},
+  indexRoute,
   {path: 'home', loadChildren: 'app/home/home.module', data: {title: 'Pokemon List', preload: true}, canLoad: [CanLoadPokemon]},
   {path: 'guards', loadChildren: 'app/guards/guards.module', data: {title: 'Router Guards', preload: true}},
   {path: 'resolver', loadChildren: 'app/resolver/resolver.module', data: {title: 'Data Resolver'}},
   {path: 'preview', loadChildren: 'app/preview/preview.module'},
-  indexRoute,
   fallbackRoute
 ];
 
 export default RouterModule.forRoot(routes, {
+  useHash: true,
  // preloadingStrategy: PreloadAllModules
   preloadingStrategy: PreloadSelectedModuledsList
 });
-
